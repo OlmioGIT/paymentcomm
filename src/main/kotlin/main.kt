29a71d5aa75de24366497d69a9049a3ad6ea73ec
OlmioGIT <olmihailoveg@gmail.com>
2022-01-13 20:13:41 +0300

    Done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,13 @@
-
+package ru.netology
 
 
 fun main() {
-
-
+    val amount = 100000
+    val commission = amount * 0.0075
+    val cent = commission % 100
+    val rub: Int = (commission / 100).toInt()
+    if (commission<=3500)
+        print("Комиссия составит 35 рублей.")
+    else
+        println("Комиссия составит $rub рублей $cent копеек.")
 }
