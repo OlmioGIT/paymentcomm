9eef6cddeb0706b2c6648ec4a3e71f4770a1649f
OlmioGIT <95240167+OlmioGIT@users.noreply.github.com>
2022-01-17 20:51:30 +0300

    CentUpdate

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,10 @@ package ru.netology
 
 
 fun main() {
-    val amount = 100000
+    val amount = 1045312
     val commission = amount * 0.0075
-    val cent = commission % 100
-    val rub: Int = (commission / 100).toInt()
+    val cent = (commission % 100).toInt()
+    val rub: Int = (commission / 100) .toInt()
     if (commission<=3500)
         print("Комиссия составит 35 рублей.")
     else
